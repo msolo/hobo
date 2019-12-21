20e6fba8e107859dcc859e28c48838fad03d1bca
Mike Solomon <msolo@gmail.com>
2019-12-21 11:00:24 -0800

    Update the boxcar-init script for ubuntu-18.04.

--- demos/make-boxcar/boxcar-init.sh
+++ demos/make-boxcar/boxcar-init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 function die() {
   echo "$@" >&2
@@ -17,7 +17,7 @@ fi
 
 # Add the default insecure key for bootstrapping and fix permissions.
 mkdir -p /home/hobo/.ssh
-curl -o /home/hobo/.ssh/authorized_keys https://raw.githubusercontent.com/msolo/hobo/master/keys/hobo-bootstrap-insecure.pub
+curl -s -o /home/hobo/.ssh/authorized_keys https://raw.githubusercontent.com/msolo/hobo/master/keys/hobo-bootstrap-insecure.pub
 chmod 700 /home/hobo/.ssh /home/hobo
 chown -R hobo:hobo /home/hobo
 chmod 600 /home/hobo/.ssh/authorized_keys
@@ -28,6 +28,15 @@ cat << EOF > /etc/sudoers.d/hobo
 hobo ALL=(ALL) NOPASSWD:ALL
 EOF
 
+# systemd dns is broken by design, kneecap it.
+rm /etc/resolv.conf
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+# fix incredibly slow sudo performance due to botched lookups
+echo "127.0.0.1 $(cat /etc/hostname)" >> /etc/hosts
+
+# Ubuntu can't be trusted. sad.
+sed -i 's/^ENABLED=.*/ENABLED=0/' /etc/default/motd-news
+
 # Disable pointless cron, but leave most intact.
 chmod -x $ROOTFS/etc/cron.daily/mlocate
 
@@ -35,15 +44,19 @@ chmod -x $ROOTFS/etc/cron.daily/mlocate
 /bin/systemctl disable apt-daily.service
 /bin/systemctl disable apt-daily.timer
 
+export DEBIAN_FRONTEND=noninteractive
+
 # Update packages
 apt-get update -qq
 apt-get -qq upgrade
+apt-get install -qq -y --install-recommends linux-generic-hwe-18.04
 
 # Maybe install useful baseline items
 # apt-get -qq -y --fix-missing --no-install-recommends -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install $DEB_LIST
 
 # Start removing unnecessary files
-apt-get remove linux-headers-* linux-firmware
+apt-get -qqy remove ntfs-3g linux-*4.15.*
+apt-get -qqy remove linux-headers-* linux-firmware landscape-common ubuntu-server cloud* vim
 apt-get -qqy autoremove --purge
 apt-get autoclean -qqy
 apt-get clean -qqy
@@ -57,6 +70,14 @@ find /var -name '*-old' -delete
 rm -rf /usr/share/man/??
 rm -rf /usr/share/man/??_*
 
+swapoff /swap.img || true
+cp /dev/null /swap.img
+
 # Make sure the disk will be as small as possible by zeroing out the unused blocks.
-e4defrag /dev/sda1
-dd if=/dev/zero of=/zero.fill bs=1024x1024; sync; rm -r /zero.fill
+e4defrag /dev/sda?
+
+dd if=/dev/zero of=/zero.fill bs=1024x1024 || true
+sync
+rm -rf /zero.fill
+
+e2label $(awk '$2 == "/" { print $1 }' < /proc/mounts) root
